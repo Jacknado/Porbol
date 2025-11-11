56aa4246edf09ee2bacd9bacb894477ff2974ed4
ian-freeman999 <ianefreeman1090+classgithub@gmail.com>
2025-11-11 11:25:39 -0500

    Added WaveTrail script

---
+++ Assets/Scripts/WaveTrail.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using UnityEngine;
+
+public class WaveTrail : MonoBehaviour
+{
+    public GameObject trail;
+    public float trailInterval = 0.1f;
+    [Header("Assign trailfolder to an empty gameobject. It will store the trail to not clutter scene")]
+    public GameObject trailFolder;
+    void Start()
+    {
+        InvokeRepeating("MakeTrail", 0, trailInterval);
+    }
+    void MakeTrail()
+    {
+        Debug.Log(gameObject.transform.position);
+        GameObject newTrail = Instantiate(trail, transform.position, transform.rotation);
+        StartCoroutine(RemoveTrail(newTrail));
+    }
+    IEnumerator RemoveTrail(GameObject newTrail)
+    {
+        yield return new WaitForSeconds(1f);
+        Destroy(newTrail);
+    }
+}
