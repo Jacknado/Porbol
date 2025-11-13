058b632f5d626b1876a2fa61030403545eb5af77
william-uin <will+coding1@ewi.ng>
2025-11-13 11:48:01 -0500

    Added explosion on demand.

--- Assets/Scripts/Powerups/ExplosionPowerup.cs
+++ Assets/Scripts/Powerups/ExplosionPowerup.cs
@@ -1,14 +1,16 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ExplosionPowerup : MonoBehaviour
 {
     public float explosionRadius = 5f;
     public LayerMask destroyableLayer;
+    public GameObject ExplosionIndicator;
 
     public void Explode(Vector3 position)
     {
         Collider[] hits = Physics.OverlapSphere(position, explosionRadius);
-
+        ExplosionIndicator.SetActive(false);
         foreach (Collider hit in hits)
         {
             if (hit is BoxCollider && ((1 << hit.gameObject.layer) & destroyableLayer) != 0)
@@ -17,7 +19,10 @@ public class ExplosionPowerup : MonoBehaviour
             }
         }
     }
-
+    void Start()
+    {
+        ExplosionIndicator = GameObject.Find("Canvas").transform.GetChild(1).gameObject;
+    }
     // Optional: visualize explosion radius in editor
     private void OnDrawGizmosSelected()
     {
