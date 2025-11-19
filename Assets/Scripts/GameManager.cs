cd51c8950ba7f42f7d4ecfa8a9af93991e37d6e8
william-uin <will+coding1@ewi.ng>
2025-11-19 11:47:27 -0500

    Removed Powerups on Respawn

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -63,6 +63,8 @@ public class GameManager : MonoBehaviour
         deathCount += 1;
         player.transform.position = new Vector3(0, 0, 0);
         player.GetComponent<WaveTrailSmooth>().RemoveTrail();
+        player.GetComponent<ExplosionPowerup>().Explode(player.transform.position);
+        player.GetComponent<MistyStep>().Disable();
         foreach (Transform child in obstacleFolder.transform)
         {
             child.gameObject.SetActive(true);
